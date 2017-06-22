deed803d909df525555b62ea771b81fc24acc75a
artemisxen <artemisxen@gmail.com>
2017-06-23 00:42:02 +0100

    8.6 exercise

--- directory.rb
+++ directory.rb
@@ -1,21 +1,21 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "- Please enter the names of the students:"
+  puts "To finish, just hit return twice."
   students = []
   name = gets.chomp
   while !name.empty? do
     if name.length < 12 && name.start_with?('s')
-      puts "Please enter the age of the students"
+      puts "- Please enter the age of the students:"
       age = gets.chomp
-      puts "Please enter the nationality"
+      puts "- Please enter the nationality:"
       nationality = gets.chomp
       students << {name: name, age: age, nationality: nationality, cohort: :november}
       puts "Now we have #{students.count} students"
-      puts "Please enter the names of the students"
+      puts "- Please enter the names of the students:"
       puts "To finish, just hit return twice"
-      name = gets.chompc
+      name = gets.chomp
     else
-      puts "Please enter the names of the students"
+      puts "- Please enter the names of the students:"
       puts "To finish, just hit return twice"
       name = gets.chomp
    end
@@ -24,20 +24,20 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts " The students of Villains Academy ".center(70, "* ")
+  puts "--------------------------------".center(70)
 end
 
 def print(students)
   i = 0
   while i < students.length
-    puts "#{i + 1}. #{students[i][:name]}, #{students[i][:age]}, #{students[i][:nationality]} (#{students[i][:cohort]} cohort)"
+    puts "#{i + 1}. #{students[i][:name]}, #{students[i][:age]}, #{students[i][:nationality]} (#{students[i][:cohort]} cohort)".center(70)
     i += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(70)
 end
 #nothing happens untill we call the methods
 students = input_students
