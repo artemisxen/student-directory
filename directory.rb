b03790e7ff1e507bbaf3dc2bc198ace7d173a387
artemisxen <artemisxen@gmail.com>
2017-06-23 00:21:08 +0100

    8.5 exercise

--- directory.rb
+++ directory.rb
@@ -4,14 +4,22 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    if name.length < 12
-      students << {name: name, cohort: :november}
+    if name.length < 12 && name.start_with?('s')
+      puts "Please enter the age of the students"
+      age = gets.chomp
+      puts "Please enter the nationality"
+      nationality = gets.chomp
+      students << {name: name, age: age, nationality: nationality, cohort: :november}
       puts "Now we have #{students.count} students"
-      name = gets.chomp
+      puts "Please enter the names of the students"
+      puts "To finish, just hit return twice"
+      name = gets.chompc
     else
+      puts "Please enter the names of the students"
+      puts "To finish, just hit return twice"
       name = gets.chomp
-    end
    end
+ end
    students
 end
 
@@ -23,7 +31,7 @@ end
 def print(students)
   i = 0
   while i < students.length
-    puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts "#{i + 1}. #{students[i][:name]}, #{students[i][:age]}, #{students[i][:nationality]} (#{students[i][:cohort]} cohort)"
     i += 1
   end
 end
